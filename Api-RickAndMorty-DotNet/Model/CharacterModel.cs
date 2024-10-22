dfdcee18d289e989272690cdb9ca7bd59d428af4
Foqsz <contatovictorvinicius05@gmail.com>
2024-10-22 09:54:28 -0300

    Reajustando model para receber mais ao deserializar

--- Api-RickAndMorty-DotNet/Model/CharacterModel.cs
+++ Api-RickAndMorty-DotNet/Model/CharacterModel.cs
@@ -9,5 +9,17 @@
         public string Type { get; set; }
         public string Gender { get; set; }
         public DateTime Created { get; set; }
+        public Origin origin { get; set; }
+        public Location location { get; set; } 
+
+        public class Origin
+        {
+            public string Name { get; set; } 
+        }
+
+        public class Location
+        {
+            public string Name { get; set; } 
+        }
     }
 }
